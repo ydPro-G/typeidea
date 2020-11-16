336273e51380eb7ce2dbd314f64f904e5506a956
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-11-16 17:32:49 +0800

    update

--- typeidea_item/typeidea/typeidea/urls.py
+++ typeidea_item/typeidea/typeidea/urls.py
@@ -23,7 +23,7 @@ from config.views import links
 # 三个View post_list post_detail links
 urlpatterns = [
     # url(<正则>,<view function>,<默认传递参数，无论什么请求过来都会传递这个参数到view function中>,<url的名称>)
-    url(r'^$', post_list， name='index'),
+    url(r'^$', post_list, name='index'),
     # view从url中<>内取值然后作为参数传递给后面的view函数，逗号后面是view中的函数名称，这样url和view可以对应起来
     # 通过这一逻辑我们可以处理不同的url匹配到同一参数的逻辑 /category/1 or post/2.html  后面的字段是对应的view函数， <>内的字段作为参数传递给函数处理
     url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
