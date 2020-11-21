88a450e7ca62bad3ce8b75525f0ef28ef579a0eb
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-11-21 16:36:53 +0800

    修改url

--- typeidea_item/typeidea/typeidea/urls.py
+++ typeidea_item/typeidea/typeidea/urls.py
@@ -18,20 +18,25 @@ from django.contrib import admin
 
 
 from .custom_site import custom_site
-from blog.views import post_detail,post_list # 导入用来处理请求的view函数
 from config.views import links
+from blog.views import (
+    IndexView, CategoryView, TagView,
+    PostDetailView,
+)
+
 
 # 三个View post_list post_detail links
 urlpatterns = [
     # url(<正则>,<view function>,<默认传递参数，无论什么请求过来都会传递这个参数到view function中>,<url的名称>)
-    url(r'^$', post_list, name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
     # view从url中<>内取值然后作为参数传递给后面的view函数，逗号后面是view中的函数名称，这样url和view可以对应起来
     # 通过这一逻辑我们可以处理不同的url匹配到同一参数的逻辑 /category/1 or post/2.html  后面的字段是对应的view函数， <>内的字段作为参数传递给函数处理
-    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
     
     # 指定要匹配的参数pk作为过滤Post数据的参数，从而产生Post.objects.filter(pk=pk)拿到指定文章的实例 
-    url(r'^post/(?P<post_id>\d+).html$', post_detail , name='post-detail'),
+    # 如果不写as_view() 会报错误，接受一个位置参数post_id但是给了2个
+    url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view() , name='post-detail'),
     url(r'^links/$', links, name='links'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'), 
