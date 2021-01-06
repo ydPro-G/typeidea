785660ac74887f574d636037cf1102ae7670a846
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2021-01-06 15:02:09 +0800

    缓存装饰器

--- cache_decorator.py
+++ cache_decorator.py
@@ -4,4 +4,32 @@ import time
 CACHE = {}
 
 def cache_it(func):
+    # 保留原函数的签名，因为被装饰的函数实际上对外暴露的是装饰器函数。
     @functools.wraps(func)
+    def inner(*args, **kwargs):
+        # repr() 函数将对象转化为供解释器读取的形式。
+        key = repr(*args,**kwargs)
+        try:
+            result = CACHE[key]
+        except KeyError:
+            result = func(*args, **kwargs)
+            CACHE[key] = result
+        return result
+    return inner
+
+@cache_it
+def query(sql):
+    time.sleep(1)
+    result = 'execute %s' % sql
+    return result
+
+
+if __name__ == '__main__':
+    start = time.time()
+    query('SELECT * FROM blog_post')
+    print(time.time() - start)
+
+    # 运行后第二次
+    start = time.time()
+    query('SELECT * FROM blog_post')
+    print(time.time() - start)
